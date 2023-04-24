86972cfe32c3fddad832788776c9ce0208455fd5
joseph <josephmacharia575@gmail.com>
2023-04-24 16:44:11 +0300

    almost

---
+++ bank-of-flatiron/src/AccountContainer.js
@@ -0,0 +1,30 @@
+import React ,{useEffect} from "react";
+import TransactionsList from "./transactionsList";
+import Search from "./Search";
+import AddtransactionForm from"./AddTransactionForm";
+import {useState} from "react";
+
+function AccountContainer(){
+    const [transactions,setTransaction]=useState([])
+    function handleTransactions(){
+        fetch("http://localhost:8001/transactions")
+        .then((res)=>{return res.json()})
+        .then ((data)=>setTransaction(data))
+      }
+      useEffect(()=>{
+        handleTransactions();
+      },[])
+
+
+      function getFilter(e){
+        setTransaction(transactions.filter(transaction => transaction.description.toLowerCase().includes(e.target.value.toLowerCase())))
+      }
+      return (
+        <div>
+          <Search  getFilter={getFilter}/>
+          <AddTransactionForm />
+          <TransactionsList transactions={transactions} />
+        </div>
+      );
+      }
+}
---
+++ bank-of-flatiron/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div classnName="ui raised segment">
+      <div className="segment violet inverted">
+        <h2> BANK OF FLATIRON</h2>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
